15e6d641104c5cfbea36cd1304fab1d081880b1e
Himanshu <hvasishth@gmail.com>
2017-04-05 15:03:34 -0700

    Add a chat widget

--- renderer/components/CreateForm.jsx
+++ renderer/components/CreateForm.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 const CreateForm = ({createItem}) => {
   return (
     <div className='list-item'>
+      <div className='chat-widget'></div>
       <input className='create-form' onKeyPress={createItem} onClick={(event) => {event.stopPropagation()}} /> 
     </div>
   )
